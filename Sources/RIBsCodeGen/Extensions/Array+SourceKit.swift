2dc1c5be69677adc2b86faf45f14174a13be3170
Wataru343 <20026379+Wataru343@users.noreply.github.com>
2026-08-18 18:19:23 +0900

    Revert "Rename SourceKit extensions for SwiftNode"
    This reverts commit 6ccd9c6af2724955cb2539b0edec33ba494b43b7.

--- Sources/RIBsCodeGen/Extensions/Array+SourceKit.swift
+++ Sources/RIBsCodeGen/Extensions/Array+SourceKit.swift
@@ -1,5 +1,5 @@
 //
-//  Array+SwiftNode.swift
+//  Array+SourceKit.swift
 //  RIBsCodeGen
 //
 //  Created by 今入　庸介 on 2021/02/04.
--- Sources/RIBsCodeGen/Extensions/Dictionary+SourceKit.swift
+++ Sources/RIBsCodeGen/Extensions/Dictionary+SourceKit.swift
@@ -1,5 +1,5 @@
 //
-//  SwiftNode+Accessors.swift
+//  Dictionary+SourceKit.swift
 //  RIBsCodeGen
 //
 //  Created by 今入　庸介 on 2021/02/04.
